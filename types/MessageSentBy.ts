47f9d516d1ca6f57f0cd43e38ac23635e49460ed
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-31 07:01:23 -0500

    Auto sync

---
+++ types/MessageSentBy.ts
@@ -0,0 +1,1 @@
+export type MessageSentBy = 'PATIENT' | 'ADMIN' | 'SERVER'
---
+++ types/socketEvents/IRealTimeMessageEventData.ts
@@ -0,0 +1,15 @@
+import { MessageSentBy } from '../MessageSentBy'
+import { RealTimeEventData } from './RealTimeEventData'
+
+export type RealTimeMessageEventData = RealTimeEventData & {
+  realTimeEventName: 'new message'
+  id: number
+  body: string
+  createdAt: Date
+  sentBy: MessageSentBy
+  adminUserId: string | null
+  patientId: string
+  practiceId: number
+  adminFullName: string | null
+  patientFullName: string | null
+}
