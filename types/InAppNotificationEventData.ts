0f69c92cb59ae1171beebc3a631dd8c602fea910
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-01 07:40:09 -0500

    Auto sync

--- types/InAppNotificationEventData.ts
+++ types/InAppNotificationEventData.ts
@@ -2,14 +2,10 @@ import { MessageSentBy } from './MessageSentBy'
 
 export type InAppNotificationEventData = {
   notificationName: string
-  body: string
-  title: string
 } & {
   notificationName: 'new message'
   patientId: string
   sentBy: MessageSentBy
-  title: string
-  body: string
 }
 
 export type InAppNotificationEventName = InAppNotificationEventData['notificationName']
