9136dbd13f0f1d772fade173587c36440fdd1691
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-01 07:00:03 -0500

    Auto sync

--- types/realTimeEvents/AllRealTimeEvents.ts
+++
@@ -1,7 +0,0 @@
-import { RealTimeMessageEventData } from './RealTimeMessageEventData'
-
-export type RealTimeEventDatas = RealTimeMessageEventData
-export type socketEventNames = RealTimeEventDatas['socketEventName']
-export type RealTimeEventMap = {
-  [K in RealTimeEventDatas['socketEventName']]: Extract<RealTimeEventDatas, { socketEventName: K }>
-}
