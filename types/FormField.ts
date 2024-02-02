a388cb6cc49b4afa7b1d1caa3ac94db0968cdadd
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-02 15:33:20 -0500

    Auto sync

--- types/FormField.ts
+++ types/FormField.ts
@@ -1,14 +1,14 @@
 export type FormFieldType = 'DATE' | 'DROPDOWN' | 'LONG_TEXT' | 'RADIO' | 'SIGNATURE' | 'TEXT_AREA' | 'TEXT_INPUT' | 'TIME' | 'TITLE' | 'YES_NO'
 
 export type FormFieldValueTypeMap = {
-  DATE: Date
+  DATE: Date | null
   DROPDOWN: string
   LONG_TEXT: string
   RADIO: string[]
   SIGNATURE: string
   TEXT_AREA: string
   TEXT_INPUT: string
-  TIME: Date
+  TIME: Date | null
   TITLE: never
   YES_NO: boolean | null
 }
