e968a6b4cf1fe7ccecf32a7ee487ad9b419b5aeb
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-02 19:04:44 -0500

    Auto sync

--- types/FormField.ts
+++ types/FormField.ts
@@ -1,4 +1,5 @@
-import { FormFieldType, PatientResponseFormFieldType } from '../../gql/graphql'
+export type FormFieldType = 'DATE' | 'DROPDOWN' | 'LONG_TEXT' | 'RADIO' | 'SIGNATURE' | 'TEXT_AREA' | 'TEXT_INPUT' | 'TIME' | 'TITLE' | 'YES_NO'
+export type PatientResponseFormFieldType = 'DATE' | 'DROPDOWN' | 'RADIO' | 'SIGNATURE' | 'TEXT_AREA' | 'TEXT_INPUT' | 'TIME' | 'YES_NO'
 
 export function isValueFormFieldType(type: FormFieldType): type is PatientResponseFormFieldType {
   return type !== 'LONG_TEXT' && type !== 'TITLE'
